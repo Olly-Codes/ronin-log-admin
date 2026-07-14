5369f2d73c30c0dc6203a5238b03abb33b6be16c
Olly <ollycodesdev@gmail.com>
2026-07-14 21:16:57 +0200

    feat: add routes and paths to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import { useAuth } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+
 function App() {
 
+  const { loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <main className="app-content">
-      <h1>Hello World</h1>
-    </main>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route 
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
