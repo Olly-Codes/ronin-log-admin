d3e3ac19df924f3a5424d74b9b927f86427c2893
Olly <ollycodesdev@gmail.com>
2026-07-17 09:50:01 +0200

    feat: add fetch comments in comments admin page

--- src/pages/dashboard/CommentsPage.jsx
+++ src/pages/dashboard/CommentsPage.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import commentsAPI from "../../api/commentsAPI";
+
 const CommentsPage = () => {
+
+    const [comments, setComments] = useState([]);
+    const [loadingComments, setLoadingComments] = useState(true);
+
+    useEffect(() => {
+
+        const fetchComments = async () => {
+
+            try {
+                const comments = await commentsAPI.getComments();
+                setComments(comments.comments);
+                setLoadingComments(false);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchComments();
+    }, []);
+
     return (
         <section className="comments-content">
-            <h1>Comments Page</h1>
+            <h1>Comments</h1>
+            {loadingComments ? (
+                <p>Loading Comments...</p>
+            ) : (
+                <ul>
+                    {comments.map((comment) => (
+                        <li key={comment.comment_id}>
+                            <p>{comment.created_at}</p>
+                            <p>{comment.content}</p>
+                            <p>Made by {comment.username} in {comment.title}</p>
+                            <button type="button">Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </section>
     )
 };
