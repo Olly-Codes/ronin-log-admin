c0a2edbb7fd0518adbbd56001f7eb32342ba15ba
Olly <ollycodesdev@gmail.com>
2026-07-17 15:03:53 +0200

    feat: add page for new review creation

--- src/pages/dashboard/NewReviewPage.jsx
+++ src/pages/dashboard/NewReviewPage.jsx
@@ -1,7 +1,131 @@
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import mediaAPI from "../../api/mediaAPI";
+import demographicsAPI from "../../api/demographicsAPI";
+
 const NewReviewPage = () => {
+
+    const [title, setTitle] = useState("");
+
+    const [mediaType, setMediaType] = useState("");
+    const [mediaOptions, setMediaOptions] = useState([]);
+
+    const [demographic, setDemographic] = useState("");
+    const [demographicOptions, setDemographicOptions] = useState([]);
+
+    const [score, setScore] = useState("");
+    const [coverImageUrl, setCoverImageUrl] = useState("");
+    const [bodyMarkdown, setBodyMarkdown] = useState("");
+    const [submitting, setSubmitting] = useState(false);
+    const [error, setError] = useState("");
+    const [loadingData, setLoadingData] = useState(true);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            try {
+                const mediaTypes = await mediaAPI.getMediaTypes();
+                setMediaOptions(mediaTypes.mediaTypes);
+
+                const demographicData = await demographicsAPI.getDemographics();
+                setDemographicOptions(demographicData.demographics);
+                
+                setLoadingData(false);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchData();
+
+    }, []);
+
+    const handleFileUpload = (e) => {
+        console.log("Uploaded");
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        console.log("I work!");
+    };
+
     return (
         <section className="new-review-content">
-            <h1>This is the new review page</h1>
+            <h1>New Review</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="coverImage">Cover Image</label>
+                <input 
+                    type="file" 
+                    id="coverImage" 
+                    value={coverImageUrl} 
+                    onChange={handleFileUpload} 
+                />
+
+                <label htmlFor="title">Title</label>
+                <input 
+                    id="title" 
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required 
+                />
+
+                <label htmlFor="title">Media type</label>
+                {loadingData ? (
+                    <p>Loading media types...</p>
+                ) : (
+                    <select value={mediaType} onChange={(e) => setMediaType(e.target.value)}>
+                        {mediaOptions.map((type) => (
+                            <option key={type.media_type_id}>{type.media_type_id}. {type.name}</option>
+                        ))}
+                    </select>
+                )}
+
+                <label htmlFor="title">Demographic</label>
+                {loadingData ? (
+                    <p>Loading demographics...</p>
+                ) : (
+                    <select value={demographic} onChange={(e) => setDemographic(e.target.value)}>
+                        {demographicOptions.map((d) => (
+                            <option key={d.demographic_id}>{d.demographic_id}. {d.name}</option>
+                        ))}
+                    </select>
+                )}
+
+                <label htmlFor="score">Score</label>
+                <input 
+                    type="number"
+                    min={0}
+                    max={10}
+                    defaultValue={0}
+                    onChange={(e) => setScore(e.target.value)} 
+                />
+
+                <div className="body-editor">
+                    <div className="editor-wrapper">
+                        <label htmlFor="bodyMarkdown">Body (Markdown supported)</label>
+                        <textarea 
+                            name="bodyMarkdown" 
+                            id="bodyMarkdown"
+                            onChange={(e) => setBodyMarkdown(e.target.value)}
+                            required    
+                        />
+                    </div>
+
+                    <div className="preview-wrapper">
+                        <p>Body Preview</p>
+                        <Markdown>{bodyMarkdown}</Markdown>
+                    </div>
+                </div>
+
+                <div className="btn-wrapper">
+                    <button type="submit" disabled={submitting}>
+                        {submitting ? "Saving..." : "Save Review"}
+                    </button>
+                </div>
+            </form>
         </section>
     );
 };
