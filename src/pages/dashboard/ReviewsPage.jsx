c9366e8257bb385f7ca605aa30f308c48717e68f
Olly <ollycodesdev@gmail.com>
2026-07-17 09:16:35 +0200

    feat: add fetch reviews to reviews admin page

--- src/pages/dashboard/ReviewsPage.jsx
+++ src/pages/dashboard/ReviewsPage.jsx
@@ -1,7 +1,67 @@
+import { useEffect, useState } from "react";
+import reviewsAPI from "../../api/reviewsAPI";
+
 const ReviewsPage = () => {
+
+    const [reviews, setReviews] = useState([]);
+    const [loadingReviews, setLoadingReviews] = useState(true);
+
+    useEffect(() => {
+
+        const fetchReviews = async () => {
+
+            try {
+                const reviews = await reviewsAPI.getReviews();
+                setReviews(reviews.reviews);
+                setLoadingReviews(false);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchReviews();
+    }, []);
+
     return (
         <section className="reviews-content">
-            <h1>Reviews Page</h1>
+            <div className="heading-wrapper">
+                <h1>Reviews</h1>
+                <button type="button">New Review</button>
+            </div>
+            {loadingReviews ? (
+                <p>Loading reviews...</p>
+            ) : (
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Media Type</th>
+                                <th>Demographic</th>
+                                <th>Score</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {reviews.map((review) => (
+                                <tr key={review.review_id}>
+                                    <td>{review.title}</td>
+                                    <td>{review.media_type}</td>
+                                    <td>{review.demographic}</td>
+                                    <td>{review.score}</td>
+                                    <td>{review.published ? "Published" : "Draft"}</td>
+                                    <td>
+                                        <button>Edit</button>
+                                    </td>
+                                    <td>
+                                        <button>Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </section>
     )
 };
