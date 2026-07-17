79cee362f08fd430f39f4934949716731ec9112d
Olly <ollycodesdev@gmail.com>
2026-07-17 09:49:21 +0200

    feat: add api call to fetch all comments

--- src/api/commentsAPI.js
+++ src/api/commentsAPI.js
@@ -1,10 +1,16 @@
 import apiClient from "./apiClient";
 
+const getComments = async () => {
+    const res = await apiClient.get("/comments");
+    return res.data;
+};
+
 const getCommentsCount = async () => {
     const res = await apiClient.get("/comments?countOnly=true");
     return res.data;
 };
 
 export default {
+    getComments,
     getCommentsCount
 }
