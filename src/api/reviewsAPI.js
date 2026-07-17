c8ae1769e977e511e94f6a332daf4175cbd72ef9
Olly <ollycodesdev@gmail.com>
2026-07-17 08:51:55 +0200

    feat: add api call to fetch sorted reviews

--- src/api/reviewsAPI.js
+++ src/api/reviewsAPI.js
@@ -20,9 +20,15 @@ const getunPublishedReviewsCount = async () => {
     return res.data;
 };
 
+const getSortedReviews = async (sort) => {
+    const res = await apiClient.get(`/admin/reviews?sort=${sort}`);
+    return res.data;
+};
+
 export default {
     getReviews,
     getReviewsCount,
     getPublishedReviewsCount,
-    getunPublishedReviewsCount
+    getunPublishedReviewsCount,
+    getSortedReviews
 }
