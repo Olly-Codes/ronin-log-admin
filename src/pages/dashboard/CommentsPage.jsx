708de207884636f04173753bbd41e45a5a86748a
Olly <ollycodesdev@gmail.com>
2026-07-18 18:55:39 +0200

    feat: add handle delete for a comment

--- src/pages/dashboard/CommentsPage.jsx
+++ src/pages/dashboard/CommentsPage.jsx
@@ -5,6 +5,7 @@ const CommentsPage = () => {
 
     const [comments, setComments] = useState([]);
     const [loadingComments, setLoadingComments] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
 
@@ -22,23 +23,44 @@ const CommentsPage = () => {
         fetchComments();
     }, []);
 
+    const handleDelete = async (id) => {
+        setError("");
+
+        try {
+            await commentsAPI.deleteComment(id);
+            setComments((prev) => prev.filter((c) => c.comment_id !== id));
+        } catch (err) {
+            setError("Could not delete comment");
+        }
+    };
+
     return (
         <section className="comments-content">
-            <h1>Comments</h1>
-            {loadingComments ? (
-                <p>Loading Comments...</p>
-            ) : (
-                <ul>
-                    {comments.map((comment) => (
-                        <li key={comment.comment_id}>
-                            <p>{comment.created_at}</p>
-                            <p>{comment.content}</p>
-                            <p>Made by {comment.username} in {comment.title}</p>
-                            <button type="button">Delete</button>
-                        </li>
-                    ))}
-                </ul>
-            )}
+            <div className="heading-wrapper">
+                <h1>Comments</h1>
+            </div>
+            <div className="comments-wrapper">
+                {error && <p>{error}</p>}
+                {loadingComments ? (
+                    <p>Loading Comments...</p>
+                ) : (
+                    <ul>
+                        {comments.map((comment) => (
+                            <li key={comment.comment_id}>
+                                <p>{comment.created_at}</p>
+                                <p>{comment.content}</p>
+                                <p>Made by {comment.username} in {comment.title}</p>
+                                <button 
+                                    type="button"
+                                    onClick={() => handleDelete(comment.comment_id)}    
+                                >
+                                    Delete
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </section>
     )
 };
