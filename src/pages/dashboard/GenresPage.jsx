69996a843e5cde86cd1e8e5811385024d7198a74
Olly <ollycodesdev@gmail.com>
2026-07-18 17:45:24 +0200

    feat: add genres call and display data to genres page

--- src/pages/dashboard/GenresPage.jsx
+++ src/pages/dashboard/GenresPage.jsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from "react";
+import genreAPI from "../../api/genreAPI";
+
 const GenresPage = () => {
+
+    const [genres, setGenres] = useState([]);
+    const [loadingGenres, setLoadingGenres] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+
+        const fetchGenres = async () => {
+
+            try {
+                const genreData = await genreAPI.getGenres();
+                setGenres(genreData.genres);
+                setLoadingGenres(false);
+            } catch (err) {
+                console.log(err);
+                setError("Could not load genres");
+            }
+        };
+
+        fetchGenres();
+    }, []);
+
     return (
         <section className="genres-content">
-            <h1>Genres Page</h1>
+            <div className="heading-wrapper">
+                <h1>Genres</h1>
+            </div>
+            <div className="genres-wrapper">
+                {loadingGenres ? (
+                    <p>Loading genres...</p>
+                ) : (
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Genre</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {genres.map((genre) => (
+                                    <tr key={genre.genre_id}>
+                                        <td>{genre.name}</td>
+                                        <td>
+                                            <button type="button">Edit</button>
+                                        </td>
+                                        <td>
+                                            <button type="button">Delete</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
         </section>
     )
 };
