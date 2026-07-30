8e24be863544365127a72adf4d3fbb4b3cddb56c
Olly <ollycodesdev@gmail.com>
2026-07-30 19:40:35 +0200

    feat: add react-icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router";
 import { useAuth } from "../context/AuthContext";
+import { 
+    MdOutlineDashboard, 
+    MdOutlinePeople, 
+    MdOutlineRateReview, 
+    MdOutlineComment, 
+    MdOutlineCategory 
+} from "react-icons/md";
 
 const Sidebar = () => {
 
     const { logout } = useAuth();
 
     const links = [
-        { to: "/admin/dashboard", label: "Overview" },
-        { to: "/admin/dashboard/users", label: "Users" },
-        { to: "/admin/dashboard/reviews", label: "Reviews" },
-        { to: "/admin/dashboard/comments", label: "Comments" },
-        { to: "/admin/dashboard/genres", label: "Genres" }
+        { icon: <MdOutlineDashboard />, to: "/admin/dashboard", label: "Overview" },
+        { icon: <MdOutlinePeople />, to: "/admin/dashboard/users", label: "Users" },
+        { icon: <MdOutlineRateReview />, to: "/admin/dashboard/reviews", label: "Reviews" },
+        { icon: <MdOutlineComment />, to: "/admin/dashboard/comments", label: "Comments" },
+        { icon: <MdOutlineCategory />, to: "/admin/dashboard/genres", label: "Genres" }
     ];
 
     return (
@@ -27,8 +34,9 @@ const Sidebar = () => {
                         <Link
                             key={link.to}
                             to={link.to}
-                            className="px-3 py-2 text-sm font-medium text-muted hover:bg-surface-hover hover:text-accent transition-colors"
+                            className="px-3 py-2 text-md font-medium text-muted hover:bg-surface-hover hover:text-accent transition-colors flex"
                         >
+                            <span className="inline-block mr-2 text-2xl">{link.icon}</span>
                             {link.label}
                         </Link>
                     ))}
